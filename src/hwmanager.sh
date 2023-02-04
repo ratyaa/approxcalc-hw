ca17c482f22e799642fc9d44488966b3cc754d69
Ratibor Siryi <siryi.ra@phystech.edu>
2023-02-04 05:57:33 +0300

    testing

--- src/hwmanager.sh
+++ src/hwmanager.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+OPTIND=1
 SOURCE=${BASH_SOURCE[0]}
 # resolve $SOURCE until the file is no longer a symlink
 while [ -L "$SOURCE" ]; do
@@ -17,57 +18,102 @@ DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 
 INDEX=template
 path=$DIR/template
+NC='\033[0m'
+BRED='\033[1;31m'
+BBLUE='\033[1;34m'
+BYELLOW='\033[1;33m'
 
-integer_check () {
-    NAMING_ERROR="No, you don't want to use a name that contains something but numbers, it's not funny, believe me."
+function integer_check() {
     if ! [[ $1 =~ ^[0-9]+$ ]]; then
-	echo $NAMING_ERROR
-	return 0
+	echo -e "${BYELLOW}No, you don't want to use a name that contains something but numbers, it's not funny, believe me.${NC}"
+	return 1
     fi
+    echo ""
 }
 
-c_existence_check () {
-    if [[ -d $1 ]]; then
-	echo "Sorry, but this homework already exists."
-	return 0
+function c_existence_check() {
+    if [ -d $1 ]; then
+	echo -e "${BYELLOW}Sorry, but this homework already exists.${NC}"
+	return 1
     fi
+    echo ""
 }
 
-mr_existence_check () {
-    if ! [[ -d $1 ]]; then
-	echo "Sorry, but this one doesn't exist."
-	return 0
+function mr_existence_check() {
+    if ! [ -d $1 ]; then
+	echo -e "${BYELLOW}Sorry, but this one doesn't exist.${NC}"
+	return 1
     fi
+    echo ""
 }
-
 if getopts 'c:r:' OPTION; then
     case $OPTION in
 	c)
 	    INDEX=$OPTARG
-	    integer_check $INDEX
+	    intcheckv=$(integer_check $INDEX)
+	    if ! [[ $intcheckv == "" ]]; then
+		echo $intcheckv
+		return 0
+	    fi
 	    path=$DIR/$INDEX
-	    c_existence_check $path
+	    dircheckv=$(c_existence_check $path)
+	    if ! [[ $dircheckv == "" ]]; then
+		echo $dircheckv
+		return 0
+	    fi
 	    
 	    mkdir $path
-	    cp $DIR/template/* $path/
+	    cp -r $DIR/template/* $path/
+	    echo $INDEX > $path/tex/index.tex
+	    echo "$(date +'%d %B %Y г.')" > $path/tex/date.tex
 	    
-	    echo "Created $INDEX!"
+	    echo -e "${BBLUE}Successfully created hw/$INDEX.${NC}"
 	    ;;
 	r)
 	    INDEX=$OPTARG
-	    integer_check $INDEX
+	    intcheckv=$(integer_check $INDEX)
+	    if ! [[ $intcheckv == "" ]]; then
+		echo $intcheckv
+		return 0
+	    fi
 	    path=$DIR/$INDEX
-	    mr_existence_check $path
+	    dircheckv=$(mr_existence_check $path)
+	    if ! [[ $dircheckv == "" ]]; then
+		echo $dircheckv
+		return 0
+	    fi
+
+	    if [[ -z $(git -C $DIR status --porcelain) ]]
+	    then
+		rm -r $path
+		echo -e "${BBLUE}Successfully removed hw/$INDEX.${NC}"
+	    else
+		echo -e "${BRED}You can't use this option while having uncommitted changes.${NC}\n"
+		cd $DIR
+		git status
+	    fi
 	    
-	    echo "Deleted $INDEX!"
 	    return 0
 	    ;;
 	*)
 	    "raar"
 	    return 0
     esac
+else
+    INDEX=$1
+    intcheckv=$(integer_check $INDEX)
+    if ! [[ $intcheckv == "" ]]; then
+	echo $intcheckv
+	return 0
+    fi
+    path=$DIR/$INDEX
+    dircheckv=$(mr_existence_check $path)
+    if ! [[ $dircheckv == "" ]]; then
+	echo $dircheckv
+	return 0
+    fi
 fi
 
 cd $path
-echo "Moved to $INDEX!"
+echo "Moved to hw/$INDEX."
 
