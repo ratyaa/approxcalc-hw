1e58e09dfa22d12d1303f035e78ef15a959142b5
Ratibor Siryi <siryi.ra@phystech.edu>
2023-02-05 01:11:29 +0300

    rewrote management script

--- src/hwmanager.sh
+++ src/hwmanager.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 NIXOS=1
-
 OPTIND=1
+
 SOURCE=${BASH_SOURCE[0]}
 # resolve $SOURCE until the file is no longer a symlink
-while [ -L "$SOURCE" ]; do
+while [ -L $SOURCE ]; do
     TARGET=$(readlink "$SOURCE")
     if [[ $TARGET == /* ]]; then
 	SOURCE=$TARGET
@@ -17,105 +17,74 @@ while [ -L "$SOURCE" ]; do
     fi
 done
 DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+INDEX=$(find $DIR -maxdepth 1 -type d -regex "$DIR/[0-9]*" -exec basename {} \; | sort -n | tail -1)
+if [ -z $INDEX ]; then INDEX=0; fi
+INDEX=$(($INDEX+1))
 
-INDEX=template
-path=$DIR/template
+
 NC='\033[0m'
 BRED='\033[1;31m'
 BBLUE='\033[1;34m'
 BYELLOW='\033[1;33m'
 
-function integer_check() {
-    if ! [[ $1 =~ ^[0-9]+$ ]]; then
-	echo -e "${BYELLOW}No, you don't want to use a name that contains something but numbers, it's not funny, believe me.${NC}"
-	return 1
-    fi
-    echo ""
+function usage() {
+    echo -e "Usage: ${SOURCE/$DIR/.} [-r/-c <string>]"
+    echo "-c -- create"
+    echo "-r -- remove"
 }
 
-function c_existence_check() {
-    if [ -d $1 ]; then
-	echo -e "${BYELLOW}Sorry, but this homework already exists.${NC}"
-	return 1
-    fi
-    echo ""
+function create() {
+    mkdir $DIR/$INDEX
+    rsync -a --exclude='.build' --exclude='svg-inkscape' --exclude 'pic' $DIR/template/ $DIR/$INDEX
+    echo $INDEX > $DIR/$INDEX/tex/index.tex
+    echo "$(date +'%d %B %Y г.')" > $DIR/$INDEX/tex/date.tex
+    echo "${BBLUE}Successfully created hw/$INDEX.${NC}"
 }
 
-function mr_existence_check() {
-    if ! [ -d $1 ]; then
-	echo -e "${BYELLOW}Sorry, but this one doesn't exist.${NC}"
-	return 1
-    fi
-    echo ""
-}
-if getopts 'c:r:' OPTION; then
-    case $OPTION in
-	c)
-	    INDEX=$OPTARG
-	    intcheckv=$(integer_check $INDEX)
-	    if ! [[ $intcheckv == "" ]]; then
-		echo $intcheckv
-		return 0
-	    fi
-	    path=$DIR/$INDEX
-	    dircheckv=$(c_existence_check $path)
-	    if ! [[ $dircheckv == "" ]]; then
-		echo $dircheckv
-		return 0
-	    fi
-	    
-	    mkdir $path
-	    cp -r $DIR/template/* $path/
-	    echo $INDEX > $path/tex/index.tex
-	    echo "$(date +'%d %B %Y г.')" > $path/tex/date.tex
-	    
-	    echo -e "${BBLUE}Successfully created hw/$INDEX.${NC}"
-	    ;;
-	r)
-	    INDEX=$OPTARG
-	    intcheckv=$(integer_check $INDEX)
-	    if ! [[ $intcheckv == "" ]]; then
-		echo $intcheckv
-		return 0
-	    fi
-	    path=$DIR/$INDEX
-	    dircheckv=$(mr_existence_check $path)
-	    if ! [[ $dircheckv == "" ]]; then
-		echo $dircheckv
-		return 0
-	    fi
-
-	    if [[ -z $(git -C $DIR status --porcelain) ]]
-	    then
-		rm -r $path
-		echo -e "${BBLUE}Successfully removed hw/$INDEX.${NC}"
-	    else
-		echo -e "${BRED}You can't use this option while having uncommitted changes.${NC}\n"
-		cd $DIR
-		git status
-	    fi
-	    
-	    return 0
-	    ;;
-	*)
-	    "raar"
-	    return 0
-    esac
-else
-    INDEX=$1
-    path=$DIR/$INDEX
-    dircheckv=$(mr_existence_check $path)
-    if ! [[ $dircheckv == "" ]]; then
-	echo $dircheckv
+function remove() {
+    if [ $(is_exist $DIR/$INDEX) = false ]; then
+	echo "${BYELLOW}Sorry, but this one doesn't exist.${NC}"
 	return 0
     fi
+
+    if ! [ -z "$(git -C $DIR status --porcelain)" ]; then
+	echo "${BRED}You can't use this option while having uncommitted changes.${NC}\n"
+	return 0
+    fi
+    rm -r $DIR/$INDEX
+    echo "${BBLUE}Successfully removed hw/$INDEX.${NC}"
+    return 0
+}
+
+function is_exist() {
+    if [ -d $1 ]; then
+	echo "true"; return 0
+    fi
+    echo "false"; return 0
+}
+
+usagep=true
+createp=false
+removep=false
+
+if getopts ':cr:' OPTION; then
+    case $OPTION in
+	c)
+	    createp=true
+	    usagep=false
+	    ;;
+	r)
+	    removep=true
+	    usagep=false
+	    INDEX=$OPTARG
+	    ;;
+    esac
 fi
 
-cd $path
-echo "Moved to hw/$INDEX."
-
-if [[ $NIXOS == 1 ]]; then
-    if [ -z $IN_NIX_SHELL ]; then
-	nix-shell $DIR
-    fi
+if [ $usagep = true ]; then
+    usage
+elif [ $removep = true ]; then
+    echo -e $(remove)
+elif [ $createp = true ]; then
+    echo -e $(create)
 fi
